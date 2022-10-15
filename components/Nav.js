08cd031d5e61371fe87d953fb1e594393ae1d1db
Rocio Flo <rocioflorencioytal@gmail.com>
2022-10-15 13:33:57 +0200

    Add components

---
+++ components/Nav.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Nav() {
+    return(
+        
+        <nav className='flex justify-between items-center py-10'>
+            <Link href='/'>
+            <button className='text-lg font-medium'>Creative Minds</button>
+            </Link>
+            <ul className='flex items-center gap-10'>
+                <Link href={'/auth/login'}>
+                    <a className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'>Join Now</a>
+                </Link>
+            </ul>
+        </nav>
+    )
+}
---
+++ components/layout.js
@@ -0,0 +1,11 @@
+import Nav from './Nav'
+
+export default function Layout({}) {
+    return (
+        <div className='mx-6 md:max-w-2x1 md-mx-auto font-poppins'>
+            <Nav/>
+            <main></main>
+        </div>
+    )
+
+}
